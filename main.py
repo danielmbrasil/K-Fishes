6eef6abf0f294c02e1da74b0a626d34579960b83
gabriel <gabriel utzig>
2022-08-21 18:37:05 -0300

    final version

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Resize not giving a shit about aspect ratio
 def resize(img, w, h):
@@ -26,73 +25,58 @@ def k_means(img):
     seg_img = seg_img.reshape(img.shape)
     return seg_img
 
-
-def histogram(image, nbins):
-    N, M = image.shape
-    hist = np.zeros(nbins).astype(int)
-    for x in range(N):
-        for y in range(M):
-            hist[image[x,y]] += 1
-    return hist
-
-def histogram_equalization(image, values):
-    hist = histogram(image, values)
-
-    histC = np.zeros(hist.shape).astype(int)
-
-    histC[0] = hist[0]
-    for i in range(1, values):
-        histC[i] = hist[i] + histC[i-1]
-  
-    N, M = image.shape
-    
-    hist_transform = np.zeros(values).astype(np.uint8)
-
-    image_eq = np.zeros(image.shape).astype(np.uint8)
-
-    for r in range(values): 
-        s = ((values-1)/float(M*N))*histC[r]
-        image_eq[ np.where(image == r) ] = s
-        hist_transform[r] = s
-    
-    return (image_eq, hist_transform)
-
-def plot_histogram(hist, name='', xlabel='Intensity', ylabel='Frequency'):
-    plt.bar(range(256), hist)
-    plt.title(name)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
-
 images = os.listdir('dataset/')
 
 for image in images:
     img = cv2.imread('dataset/'+image)
+    img = resize(img, 128, 128)
+    img_b = img
+    cv2.imwrite('results/res-' + image, img_b)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = resize(img, 50, 50)
     seg_image = k_means(img)
     
+    # split color channels and convert image to grayscale
     (B, G, R) = cv2.split(seg_image)
     gray_image = 0.2989 * R + 0.587 * G + 0.1141 * B # as defined in the article
 
+    # Histogram equalization
     gray_image = cv2.equalizeHist(np.uint8(gray_image))
-    cv2.imwrite('results/he'+image, gray_image)
-    cv2.imwrite('results/seg'+image, seg_image)
-
-    #img_eq, img_t = histogram_equalization(B, values=256)
-    #histeq_img = histogram(img_eq, 256)
-    #cv2.imwrite('teste/he_old'+image, img_eq)
-    #cv2.imshow("",img_eq)
+    #cv2.imwrite('results/he'+image, gray_image)
+    #cv2.imwrite('results/seg'+image, seg_image)
     
-    clahe = cv2.createCLAHE(clipLimit = 5)
-    final_img = clahe.apply(gray_image) + 30
-    cv2.imwrite('results/final'+image, final_img)
+    # CLAHE
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(32,32))
+    gray_image = clahe.apply(gray_image)
+    #cv2.imwrite('results/final'+image, gray_image)
+    
+    # convert image to binary (black and white)
+    im_binary_th = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)[1]
+    
+    # morphology process - opening
+    final_img = cv2.erode(im_binary_th,np.ones((5,5), np.uint8),iterations = 1)
+    final_img = cv2.dilate(final_img,np.ones((5,5), np.uint8),iterations = 1)
+    morpho = cv2.morphologyEx(im_binary_th, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
+    
+    # closing
+    morpho = cv2.morphologyEx(morpho, cv2.MORPH_CLOSE, np.ones((5,5),np.uint8))
 
-    _, im_binary_th = cv2.threshold(final_img, 127, 255, cv2.THRESH_BINARY)   
-    cv2.imwrite('results/bin'+image, im_binary_th)
-    #plot_histogram(histeq_img, 'Equalized histogram ')
-    #plt.imshow(seg_image)
-    #plt.show()
-    #cv2.imwrite('results/'+image, seg_image)
+    # edge detection using Canny
+    edges = cv2.Canny(image=morpho, threshold1=100, threshold2=200)
+#    cv2.imwrite('results/bin-' + image, morpho)
+    cv2.imwrite('results/edge-' + image, edges)
+    
+    # draw edge around detected objects in original image
+    contours = cv2.findContours(edges, 
+                            cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img_b, contours[0], -1, (0,0,255), thickness = 2)
 
+    # final result
+    cv2.imwrite('results/result-' + image, img_b)
+
+    # binary image
+    #cv2.imwrite('results/bin-' + image, im_binary_th)
+
+    # k-means segmented image
+    #cv2.imwrite('results/' + image, seg_image)
 
